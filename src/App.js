790e688daa45864b738c3d9a1a8137ecd4e37fc5
Lauren McCoy <lm_lutec@outlook.com>
2020-12-05 16:07:43 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import './App.css';
 import ToDoContainer from './containers/ToDoContainer'
 
@@ -22,8 +23,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
